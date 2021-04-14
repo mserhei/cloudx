52719a748f781cc21238c6425e261dae0ed75605
mserhei <serheimihailovjob@gmail.com>
2021-04-14 21:20:17 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import './sass/main.scss';
-import './js/utils/swiper';
+// import './js/utils/swiper';
 import './js/main.js';
-import 'material-design-icons/iconfont/material-icons.css';
+// import 'material-design-icons/iconfont/material-icons.css';
 import './robots.txt';
+
+import jsonFile from './my.json';
+import hbsFile from './my.hbs';
+import { divide } from 'lodash';
+// import { render } from 'node-sass';
+
+let lang = "en";
+
+(draw)()
+
+function draw () {
+  const item = hbsFile(jsonFile[lang]);
+  const root = document.getElementById('root');
+  root.classList.add('hide');
+  setTimeout(() => {
+    root.innerHTML = item;
+    root.classList.remove('hide')
+  }, 200);
+
+}
+
+
+
+
+
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  lang = lang === 'en' ? 'ru' : 'en';
+  console.log(lang);
+  draw();
+})
+
+
+
+
+window.onload = () => {
+  console.log('content loaded') }
--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,47 @@
-import listenClicks from './events/clickListener';
-import listenChange from './events/changeListener';
-import listenResize from './events/resizeListener';
-import listenFocusout from './events/focusoutListener';
-import listenFocusin from './events/focusinListener';
-import listenSubmit from './events/submitListener';
-import listenKeyDown from './events/keyDownListener';
-const { debounce, throttle } = require('lodash');
+// import listenClicks from './events/clickListener';
+// import listenChange from './events/changeListener';
+// import listenResize from './events/resizeListener';
+// import listenFocusout from './events/focusoutListener';
+// import listenFocusin from './events/focusinListener';
+// import listenSubmit from './events/submitListener';
+// import listenKeyDown from './events/keyDownListener';
+// const { debounce, throttle } = require('lodash');
 
-import myAgent from './utils/checkAgent';
+// import myAgent from './utils/checkAgent';
 
-const scrollEvents = () => import('./events/scrollListener');
-const mouseEvents = () => import('./events/mouseListener');
-const swipeAdd = () => import('./utils/swiper');
-const swipeEvents = () => import('./events/swipeListener');
+// const scrollEvents = () => import('./events/scrollListener');
+// const mouseEvents = () => import('./events/mouseListener');
+// const swipeAdd = () => import('./utils/swiper');
+// const swipeEvents = () => import('./events/swipeListener');
 
-const rootRel = document.getElementById('root');
+// const rootRel = document.getElementById('root');
 
-decideToDispatchIfMobile();
+// decideToDispatchIfMobile();
 
-document.addEventListener('click', listenClicks);
-document.addEventListener('change', listenChange);
-document.addEventListener('focusout', listenFocusout);
-document.addEventListener('focusin', listenFocusin);
-document.addEventListener('submit', listenSubmit);
-window.addEventListener('keydown', listenKeyDown);
-window.addEventListener('resize', listenResize);
-document.oncontextmenu = function (e) {
-  return false;
-};
+// document.addEventListener('click', listenClicks);
+// document.addEventListener('change', listenChange);
+// document.addEventListener('focusout', listenFocusout);
+// document.addEventListener('focusin', listenFocusin);
+// document.addEventListener('submit', listenSubmit);
+// window.addEventListener('keydown', listenKeyDown);
+// window.addEventListener('resize', listenResize);
+// document.oncontextmenu = function (e) {
+//   return false;
+// };
 
-async function decideToDispatchIfMobile() {
-  if (!myAgent) {
-    const movePad = await scrollEvents();
-    const listenMouse = await mouseEvents();
+// async function decideToDispatchIfMobile() {
+//   if (!myAgent) {
+//     const movePad = await scrollEvents();
+//     const listenMouse = await mouseEvents();
 
-    rootRel.addEventListener('scroll', throttle(movePad.default), 100);
-    document.addEventListener('mousemove', throttle(listenMouse.default), 20);
-    document.addEventListener('dblclick', listenMouse.changeMouse);
-  } else {
-    const addSwipeEvent = await swipeAdd();
-    addSwipeEvent.default(window, document);
+//     rootRel.addEventListener('scroll', throttle(movePad.default), 100);
+//     document.addEventListener('mousemove', throttle(listenMouse.default), 20);
+//     document.addEventListener('dblclick', listenMouse.changeMouse);
+//   } else {
+//     const addSwipeEvent = await swipeAdd();
+//     addSwipeEvent.default(window, document);
 
-    const listenSwipes = await swipeEvents();
-    document.addEventListener('swiped', listenSwipes.default);
-  }
-}
+//     const listenSwipes = await swipeEvents();
+//     document.addEventListener('swiped', listenSwipes.default);
+//   }
+// }
