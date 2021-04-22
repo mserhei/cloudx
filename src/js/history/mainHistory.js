035d8114da55e7263d248f248be2958004e37eb9
mserhei <serheimihailovjob@gmail.com>
2021-04-23 00:32:20 +0300

    commit

--- src/js/history/mainHistory.js
+++ src/js/history/mainHistory.js
@@ -49,49 +49,7 @@ if (routIdx === -1) {
   homePage();
 }
 
-const langBtn = document.querySelector('.lang-btn')
 
-function langBtnClick () {
-
-  console.log('routs1' ,routs)
-
-  console.log('langBtn click')
-  let currentPathC = window.location.pathname;
-  let currentSearchC = window.location.search;
-  let currentHashC = window.location.hash;
-  let currentAllPathC = currentPathC + currentSearchC + currentHashC;
-  console.log('currentAllPathC', currentAllPathC)
-
-
-
-  let routIdx = null;
-  routs.forEach((rout, i) => {
-    console.log('rout path:  ', rout.path)
-
-    if (currentAllPathC === rout.path) {
-
-      routIdx = i;
-    }
-  })
-
-
-
-
-  console.log('routIdx:  ', routIdx);
-  console.log('lang1', lang)
-  const nextLang = lang === 'ru' ? 'en' : 'ru';
-  changeLang(nextLang);
-  console.log('lang2', lang)
-
-
-  currentSearchC = `?lang=${lang}`;
-  const newAllPathC = currentPathC + currentSearchC + currentHashC;
-  history.replaceState(null, null, newAllPathC);
-
-  routs[routIdx].comp();
-
-  console.log('routs2' ,routs);
-}
 
 
 
--- src/js/pages/header.js
+++ src/js/pages/header.js
@@ -1,8 +1,12 @@
+import {lang, routs, changeLang} from '../history/routs';
+
+
 const header = document.getElementById('header');
 
 header.addEventListener('click', function (e) {
 
   if (e.target.getAttribute('href')) {
+    e.preventDefault();
     const link = e.target.getAttribute('href');
     const nextPath = `${link}?lang=${lang}`;
     history.pushState(null, null, nextPath);
@@ -10,8 +14,52 @@ header.addEventListener('click', function (e) {
       if (rout.path === nextPath) {
         rout.comp()
       }
-      e.preventDefault();
+
     })
   }
 
+  if (e.target.classList.contains('lang-btn')) {
+    langBtnClick()
+  }
+
+
+
+
+
 })
+
+function langBtnClick () {
+
+  console.log('routs1' ,routs)
+
+  console.log('langBtn click')
+  let currentPathC = window.location.pathname;
+  let currentSearchC = window.location.search;
+  let currentHashC = window.location.hash;
+  let currentAllPathC = currentPathC + currentSearchC + currentHashC;
+  console.log('currentAllPathC', currentAllPathC)
+
+  let routIdx = null;
+  routs.forEach((rout, i) => {
+    console.log('rout path:  ', rout.path)
+
+    if (currentAllPathC === rout.path) {
+
+      routIdx = i;
+    }
+  })
+
+  console.log('routIdx:  ', routIdx);
+  console.log('lang1', lang)
+  const nextLang = lang === 'ru' ? 'en' : 'ru';
+  changeLang(nextLang);
+  console.log('lang2', lang)
+
+  currentSearchC = `?lang=${lang}`;
+  const newAllPathC = currentPathC + currentSearchC + currentHashC;
+  history.replaceState(null, null, newAllPathC);
+
+  routs[routIdx].comp();
+
+  console.log('routs2' ,routs);
+}
