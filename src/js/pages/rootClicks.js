c2f0e9b502550c73aca7d69cae1ee2475acc014a
mserhei <serheimihailovjob@gmail.com>
2021-04-25 13:35:56 +0300

    commit

--- src/js/pages/rootClicks.js
+++ src/js/pages/rootClicks.js
@@ -5,23 +5,16 @@ root.addEventListener('click', rootClicks);
 function rootClicks (e) {
 
   if (e.target.classList.contains('home-services__title')) {
-    console.log('item click')
+
     const tabsList = document.querySelectorAll('.home-services__tabs');
     const titles = document.querySelectorAll('.home-services__title');
 
-    console.log(titles)
-    console.log(e.target)
-    // console.log(titles.indexOf(e.target))
-
-    titles.forEach((item, i) => {
-      if (item == e.target) {
-        console.log('i', i)
-      }
-    })
-
+    const titleIdx = e.target.dataset.index;
 
     titles.forEach(item => item.classList.remove('active'));
+    titles[titleIdx].classList.add('active');
     tabsList.forEach(item => item.classList.remove('active'));
+    tabsList[titleIdx].classList.add('active');
 
       }
   }
